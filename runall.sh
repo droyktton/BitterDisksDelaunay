792b886bb4433935bcfb5b457e15f97fcd219add
droyktton@gmail.com <alejandro.kolton@gmail.com>
2026-09-05 11:14:37 -0300

    conversion de pdf a eps en runall

--- runall.sh
+++ runall.sh
@@ -1,1 +1,8 @@
+#procesamos todos los .dat en carpeta data/
 for i in data/*.dat; do bash run.sh $i; done
+
+#convertirmos todos los pdf en paper_figures/ a eps
+
+cd paper_figures/
+for i in *.pdf; do pdftops -eps "$i" "${i%.pdf}.eps"; done
+cd ../
