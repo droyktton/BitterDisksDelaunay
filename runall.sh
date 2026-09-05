48184bf6c531dfe53e81dcc3264d16c1754a718e
droyktton@gmail.com <alejandro.kolton@gmail.com>
2026-09-05 11:26:48 -0300

    ahora tambien en jpg, en paper_figures

--- runall.sh
+++ runall.sh
@@ -1,8 +1,14 @@
 #procesamos todos los .dat en carpeta data/
 for i in data/*.dat; do bash run.sh $i; done
 
-#convertirmos todos los pdf en paper_figures/ a eps
 
 cd paper_figures/
+
+# convertimos todos los pdf a eps
 for i in *.pdf; do pdftops -eps "$i" "${i%.pdf}.eps"; done
+
+# convertimos todos los pdf a jpg
+for f in *.pdf; do pdftoppm -jpeg -singlefile -r 300 "$f" "${f%.pdf}"; done
+
 cd ../
+
